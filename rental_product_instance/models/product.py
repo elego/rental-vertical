b42fa76e7171ef9bb9d91d21b30a4c865cf16ea6
Yu Weng <yweng@elegosoft.com>
2020-01-28 17:08:41 +0100

    [IMP] add some product instance special fields

--- rental_product_instance/models/product.py
+++ rental_product_instance/models/product.py
@@ -29,6 +29,30 @@ class ProductProduct(models.Model):
 
     instance_serial_number_id = fields.Many2one('stock.production.lot', 'Serial Number', ondelete='set null', domain="[('product_id', '=', id)]")
     instance_current_location_id = fields.Many2one('stock.location', string="Current Location")
+    instance_state = fields.Selection(string="State",
+        selection=[
+            ('available', 'Available'),
+            ('rental', 'Rental'),
+            ('reserved', 'Reserved'),
+            ('maintenance', 'Maintenance'),
+            ('repair', 'Repair'),
+            ('delivery', 'Delivery'),
+        ], compute="_compute_instance_state")
+    instance_current_condition = fields.Char("Current Hours / Kilometers")
+    instance_next_service_date = fields.Date("Next Service")
+
+    def _compute_instance_state(self):
+        timeline_obj = self.env['product.timeline']
+        today = fields.Date.today()
+        for product in self:
+            product.instance_state = 'available'
+            timelines = timeline_obj.search([
+                ('product_id', '=', product.id),
+                ('date_start', '<=', today),
+                ('date_end', '>=', today),
+            ])
+            if timelines:
+                product.instance_state = timelines[0].type
 
     @api.onchange('product_instance')
     def onchange_product_instance(self):
