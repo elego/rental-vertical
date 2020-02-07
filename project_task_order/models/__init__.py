71ec28b03fabf22d05a84c240e304deec911b7cd
Yu Weng <yweng@elegosoft.com>
2020-02-07 11:20:16 +0100

    [DEL] delete module project_task_order, sale_rental_menu, swrent_document_extension, swrent_product_extension and swrent_quality_control

--- project_task_order/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of rental-vertical See LICENSE file for full copyright and licensing details.
-
-from . import project
-from . import product
-from . import repair
-from . import maintenance
--- swrent_product_extension/__init__.py
+++
@@ -1,1 +0,0 @@
-# Part of rental-vertical See LICENSE file for full copyright and licensing details.
