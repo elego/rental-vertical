f37348b11653458631ea9a9b6ec89da386d5b8cd
Maria Sparenberg <maria.sparenberg@elegosoft.com>
2020-02-11 10:07:42 +0100

    issue #3468 add German translation, add help texts and fix formatting

--- rental_invoice_report/models/sale_order.py
+++ rental_invoice_report/models/sale_order.py
@@ -6,4 +6,8 @@ from odoo import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    branch_name = fields.Char(string="Name of Branch")
+    branch_name = fields.Char(
+        string="Name of Branch",
+        help="Please enter here the name of the company branch. "
+             "It will be printed on report.",
+    )
