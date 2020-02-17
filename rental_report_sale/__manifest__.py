0de1a8736ced48d6b545d26c185efb04e1007bd1
cpatel <cpatel@elegosoft.com>
2020-02-17 17:24:25 +0100

    [IMP][ADD] todo points of report     1. Add a char field 'CEO' to company and use this instead of hard coding the ceo names.     2. If an invoice is in state draft, it should say so.     3. If an invoice is of type 'out_refund' / 'in_refund', it should say so.     4. Add report for sale order, too

--- rental_report_sale/__manifest__.py
+++ rental_report_sale/__manifest__.py
@@ -1,23 +1,21 @@
 # Part of rental-vertical See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Rental Report Base',
+    'name': 'Rental Report Sale',
     'version': '12.0.1.0.0',
-    'summary': 'Rental Report Base',
+    'summary': 'Rental Report Sale',
     'author': 'Elego Software Solutions Gmbh',
     'category': 'Reporting',
     'description': """
-        This module provides a customized external layout.
+        This module provides a customized sale report.
         """,
     'depends': [
-        'web',
-        'rental_base',
+        'rental_report_base',
+        'sale',
     ],
     'data': [
-        'views/report_template.xml',
-        'data/report_layout.xml',
-        'data/system_settings.xml',
-        'views/company_view.xml',
+        'report/sale_report_templates.xml',
+        'report/sale_report.xml',
     ],
     'demo': [
     ],
