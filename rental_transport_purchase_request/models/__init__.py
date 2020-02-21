e3e76eb7a88b2cce6393cc177a78a3a2ec93887d
Yu Weng <yweng@elegosoft.com>
2020-02-21 18:34:03 +0100

    [ADD] module rental_transport_purchase_request

---
+++ rental_transport_purchase_request/models/__init__.py
@@ -0,0 +1,6 @@
+# Part of rental-vertical See LICENSE file for full copyright and licensing details.
+
+from . import account_incoterms
+from . import product
+from . import purchase
+from . import sale
