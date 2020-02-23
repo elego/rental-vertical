690c4d9c14b44e8623b5f2fb8e7557c2dcdb8577
Kay Häusler <kay.haeusler@elego.de>
2020-02-23 20:49:28 +0100

    add the addon rental_purchase_order_type; issue #3462

---
+++ rental_purchase_order_type/__manifest__.py
@@ -0,0 +1,23 @@
+# Part of rental-vertical See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Rental Purchase Order Type',
+    'summary': '''Additional purchase order types for rental''',
+    'version': '12.0.1.0.0',
+    'category': 'Purchase Management',
+    'author': 'Elego Software Solutions GmbH',
+    'depends': [
+        'purchase_order_type',
+        'rental_base',
+    ],
+    'data': [
+        'data/purchase_order_type.xml',
+        'views/menu_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'application': False,
+    'license': 'AGPL-3',
+}
