15577b0b3f7776125d6453b41a247f5008aeba37
Yu Weng <yweng@elegosoft.com>
2020-02-28 19:07:25 +0100

    [ADD] module stock_shipment_management

---
+++ stock_shipment_management/__manifest__.py
@@ -0,0 +1,22 @@
+# Part of rental-vertical See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Stock Shipment Management',
+    'summary': 'Stock Shipment Management',
+    'version': '12.0.1.0.0',
+    'category': 'Transportation',
+    'author': 'Elego Software Solutions Gmbh',
+    'depends': [
+        'delivery',
+        'rental_transit_route',
+    ],
+    'data': [
+        'views/stock_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'application': False,
+    'license': 'LGPL-3',
+}
