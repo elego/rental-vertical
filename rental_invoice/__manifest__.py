e0f6dde9b441b359b5ac98396628121f59517dbf
Kay Häusler <kay.haeusler@elego.de>
2020-03-03 15:10:54 +0100

    add the addon rental_invoice; issue #3534

---
+++ rental_invoice/__manifest__.py
@@ -0,0 +1,25 @@
+# Part of rental-vertical See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Rental Invoice',
+    'summary': '''Rental Invoice''',
+    'description':
+'''This module add the 2-phase confirmation of incoming invoices including delegated charging to customers.
+''',
+    'version': '12.0.1.0.0',
+    'category': 'Purchase Management',
+    'author': 'Elego Software Solutions GmbH',
+    'depends': [
+        'purchase',
+    ],
+    'data': [
+        'wizard/create_customer_invoice_view.xml',
+        'views/account_invoice_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'application': False,
+    'license': 'AGPL-3',
+}
---
+++ rental_invoice/wizard/__init__.py
@@ -0,0 +1,1 @@
+from . import create_customer_invoice
