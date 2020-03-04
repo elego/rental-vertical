18aa0042fd4b73509f6586590214ee8e211d5519
Yu Weng <yweng@elegosoft.com>
2020-03-04 11:38:44 +0100

    [ADD] module rental_contract_month

---
+++ rental_contract_month/__manifest__.py
@@ -0,0 +1,24 @@
+# Part of rental-vertical See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Rental Contract Month',
+    'summary': 'Extension of module rental_contract and sale_rental_pricelist',
+    'description': '''
+Simplify the configuration of rental service product (month)
+''',
+    'version': '12.0.1.0.0',
+    'category': 'rental',
+    'author': 'OCA/Elego Software Solutions GmbH',
+    'depends': [
+        'rental_contract',
+        'sale_rental_pricelist',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
