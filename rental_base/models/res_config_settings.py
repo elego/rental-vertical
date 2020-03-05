fc3b30898dd3c02f46851e6c7fd1ccaf7129c180
Maria Sparenberg <maria.sparenberg@elegosoft.com>
2020-03-05 16:12:50 +0100

    issue #3287 fix description and help texts, add German translation

--- rental_base/models/res_config_settings.py
+++ rental_base/models/res_config_settings.py
@@ -6,21 +6,54 @@ from odoo import api, fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    module_sale_rental_pricelist = fields.Boolean("Price Extension", help="Set Rental Price with different Unit of Measure.")
-    module_rental_sale_offday = fields.Boolean("Off Days", help="Calculate the Off Days.")
-    module_sale_rental_timeline = fields.Boolean("Timelines")
-    module_rental_product_pack = fields.Boolean("Product Pack")
-    module_rental_product_variant = fields.Boolean("Product Variant")
-    module_rental_product_instance = fields.Boolean("Product Instance")
-    module_rental_product_instance_appointment = fields.Boolean("Appointment")
-    module_rental_product_set = fields.Boolean("Product Set")
-    module_rental_contract = fields.Boolean("Contract")
-    module_rental_repair = fields.Boolean("Repair Order")
-
-    #def set_values(self):
-    #super(ResConfigSettings, self).set_values()
-
-    #@api.model
-    #def get_values(self):
-    #    res = super(ResConfigSettings, self).get_values()
-    #    return res
+    module_rental_contract = fields.Boolean(
+        string="Rental Contracts",
+        help="If activated, rental contracts are automatically created "
+             "from monthly rentals for periodic invoicing.",
+    )
+    module_rental_product_instance = fields.Boolean(
+        string="Product Instance",
+        help="If activated, product variants can be used as unique "
+             "instances.",
+    )
+    module_rental_product_instance_appointment = fields.Boolean(
+        string="Appointments for Product Instances",
+        help="If activated, project tasks are automatically created "
+             "for appointments of product instances.",
+    )
+    module_rental_product_pack = fields.Boolean(
+        string="Rental Product Packs",
+        help="If activated, rental orders for product packs will "
+             "also update the stock of pack components.",
+    )
+    module_rental_product_set = fields.Boolean(
+        string="Rental Product Sets",
+        help="If activated, rental products can be grouped in a set "
+             "for usage in rental orders.",
+    )
+    module_rental_product_variant = fields.Boolean(
+        string="Product Variant",
+        help="If activated, product variants are used with several "
+             "new attributes and smartbuttons.",
+    )
+    module_rental_repair = fields.Boolean(
+        string="Repair Orders for Product Instances",
+        help="If activated, repair orders can be managed for product "
+             "instances, including the creation of account analytic lines"
+             "for the repair costs.",
+    )
+    module_rental_sale_offday = fields.Boolean(
+        string="Rental Off-Days",
+        help="If activated, off-days can be calculated for daily "
+             "rentals which are excluded from price calculation.",
+    )
+    module_sale_rental_pricelist = fields.Boolean(
+        string="Rental Prices",
+        help="If activated, rental prices can be configured for "
+             "hourly, daily or monthly rentals.",
+    )
+    module_sale_rental_timeline = fields.Boolean(
+        string="Timeline",
+        help="If activated, a timeline for all rental orders (and "
+             "others, e.g. repair orders) is shown as overview.",
+    )
