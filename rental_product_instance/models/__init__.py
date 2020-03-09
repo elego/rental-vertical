e21ca952a0db68ffd537b070f1d516f1c0fb50ba
Kay Häusler <kay.haeusler@elego.de>
2020-03-09 18:13:39 +0100

    move the constrain _check_date from rental_product_instance to rental_timeline_product_instance; issue #3409

--- rental_product_instance/models/__init__.py
+++ rental_product_instance/models/__init__.py
@@ -1,5 +1,4 @@
 # Part of rental-vertical See LICENSE file for full copyright and licensing details.
 
 from . import product
-from . import product_timeline
 from . import stock
--- rental_timeline_product_instance/models/product_timeline.py
+++ rental_timeline_product_instance/models/product_timeline.py
@@ -1,6 +1,6 @@
 # Part of rental-vertical See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, exceptions, fields, models, _
 
 
 class ProductTimeline(models.Model):
@@ -21,3 +21,22 @@ class ProductTimeline(models.Model):
     product_instance_serial_number_id = fields.Many2one(
         related='product_id.instance_serial_number_id',
     )
+
+    @api.multi
+    @api.constrains('date_start', 'date_end')
+    def _check_date(self):
+        for line in self:
+            if line.type in ['rental', 'reserved']:
+                domain = [
+                    ('date_start', '<', line.date_end),
+                    ('date_end', '>', line.date_start),
+                    ('product_id', '=', line.product_id.id),
+                    ('product_id.product_instance', '=', True),
+                    ('type', 'in', ['rental', 'reserved']),
+                    ('id', '!=', line.id),
+                ]
+                lines = self.search_count(domain)
+                if lines:
+                    msg = _('You can not have 2 timelines that overlaps on the same day.')
+                    raise exceptions.ValidationError(msg)
+
