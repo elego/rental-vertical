c97bd4bdf7c263bb5456a78580b359b9479e238e
Maria Sparenberg <maria.sparenberg@elegosoft.com>
2020-03-09 11:01:59 +0100

    issue #3462 add usage section for rental_contract

--- rental_contract/__manifest__.py
+++ rental_contract/__manifest__.py
@@ -7,6 +7,18 @@
 During longtime rentals, it is often required to write invoices in regular intervals.
 This is possible with the contract module, which is here extended to support rental
 use cases in extension to purchase and sale use cases.
+
+The module adds subtypes for contracts in order to distinguish between customer contracts, 
+customer rental contracts, vendor contracts and vendor rental contracts. 
+It is possible to add more subtypes with own sequence, which automatically sets the contract's code.
+
+If a contract is automatically created from sale order, it passes the sale order type to the contract subtype.
+''',
+    'usage': '''
+You can add new contract subtypes here:
+
+ - Invoicing > Configuration > Contract > Contract Subtypes
+ - Rentals > Configuration > Contract > Contract Subtypes
 ''',
     'version': '12.0.1.0.0',
     'category': 'rental',
