e030fd141fe9c9feaced2f62a8206ead9e226780
Maria Sparenberg <maria.sparenberg@elegosoft.com>
2020-03-09 13:54:52 +0100

    issue #3279 add description and usage section for rental_product_variant

--- rental_product_variant/__manifest__.py
+++ rental_product_variant/__manifest__.py
@@ -4,27 +4,28 @@
     'name': 'Rental Product Variant',
     'summary': 'Extends model product with several fields for rental use cases.',
     'description': '''
-Configuration options:
- - Show Vehicle Identification Number
- - Show License Plate
- - Show Initial Registration
+This module adds several fields to the product form.
 
 Additional fields:
-    further_ref [Char]: additional reference
-    qr_code [Char]: QR code
-    manu_year [Char]: year of manufacture
-    manu_id [Many2one]: product.manufacturer -- manufacturer
-    manu_type_id [Many2one]: product.manufacturer.type -- type
-    fleet_type_id [Many2one]: fleet.type -- fleet type
+ - further_ref [Char]: additional reference
+ - qr_code [Char]: QR code
+ - manu_year [Char]: year of manufacture
+ - manu_id [Many2one]: product.manufacturer -- manufacturer
+ - manu_type_id [Many2one]: product.manufacturer.type -- type
+ - fleet_type_id [Many2one]: fleet.type -- fleet type
 
-    vehicle_number [Char]: vehicle identification number
-    license_plate [Char]: license plate
-    init_regist [Date]: date of initial registration
+ - rental_order_ids [One2many]: sale.rental -- rented_product_id -- Rental Orders
+ - stock_move_ids [One2many]: stock.move -- product_id -- Stock Moves
+ - additional_info [Html]: arbitrary additional infomation
+ - dimension [Char]: dimension
 
-    rental_order_ids [One2many]: sale.rental -- rented_product_id -- Rental Orders
-    stock_move_ids [One2many]: stock.move -- product_id -- Stock Moves
-    additional_info [Html]: arbitrary additional infomation
-    dimension [Char]: dimension
+Additional fields configured and added by product category:
+ - Show Vehicle Identification Number -> vehicle_number [Char]: vehicle identification number
+ - Show License Plate -> license_plate [Char]: license plate
+ - Show Initial Registration -> init_regist [Date]: date of initial registration
+''',
+    'usage': '''
+In order to get vehicle related fields, open the product category and activate the desired checkboxes.
 ''',
     'version': '12.0.1.0.0',
     'category': 'product',
