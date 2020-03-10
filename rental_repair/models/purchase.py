bf4c2ac166fca43fd5b438369ad5c4864f64a822
Kay Häusler <kay.haeusler@elego.de>
2020-03-10 14:51:00 +0100

    add the page 'Vendor Repair orders'; issue #3296

---
+++ rental_repair/models/purchase.py
@@ -0,0 +1,9 @@
+# Part of rental-vertical See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    project_task_id = fields.Many2one('project.task', 'Task', ondelete="set null")
