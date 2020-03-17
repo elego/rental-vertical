823d4c7832996179570ec5794f80c533d69f9e51
Maria Sparenberg <maria.sparenberg@elegosoft.com>
2020-03-17 20:06:15 +0100

    issue #3589 improve sale order (line) view in rental_base module

--- rental_base/models/sale.py
+++ rental_base/models/sale.py
@@ -6,6 +6,11 @@ from odoo import api, fields, models, exceptions, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    rental_qty_uom = fields.Many2one(
+        string="Product Unit of Measure",
+        related='product_id.rented_product_id.uom_id',
+    )
+
     @api.multi
     def _prepare_invoice_line(self, qty):
         res = super(SaleOrderLine, self)._prepare_invoice_line(qty)
