abcb9ecc84ade5de5b4e69761b80b5fd71b1b52f
cpatel <cpatel@elegosoft.com>
2020-04-16 21:48:01 +0200

    [IMP] tour steps for workflow sale order to invoice for 60 days rental product instance

---
+++ test_rental_workflows/static/src/js/test_rental_tour.js
@@ -0,0 +1,202 @@
+odoo.define('rental.tour', function (require) {
+    "use strict";
+ 
+    var core = require('web.core');
+    var tour = require('web_tour.tour');
+    // var base = require("web_editor.base");
+ 
+    var _t = core._t;
+    var _wait = "1000";
+ 
+    tour.register('rental_tour', {
+        url: "/web",
+        // wait_for: base.ready(),
+        // test: true
+    }, [tour.STEPS.SHOW_APPS_MENU_ITEM, {
+        content: _t('Go to main menu of rental'),
+        trigger: '.o_app[data-menu-xmlid="rental_base.menu_rental_root"]',
+        position: 'bottom',
+        width: 200
+    }, {
+        content: _t('Go to top menu of customer'),
+        trigger: 'li a[data-menu-xmlid="rental_base.menu_top_customer"]',
+        position: 'bottom'
+    }, {
+        content: _t('Go to menu rental quotation'),
+        trigger: 'a[data-menu-xmlid="rental_base.menu_rental_quotations"]',
+        position: 'bottom'
+    }, {
+        content: _t("Let's create new Rental Order."),
+        trigger: '.o_list_button_add',
+        position: 'bottom'
+    }, {
+        content: _t("Create or select Customer"),
+        trigger: ".o_form_editable .o_field_many2one[name='partner_id'] .o_input_dropdown input",
+        position: "bottom",
+        run: 'click'
+    }, {
+        trigger: 'li a:contains("Azure Interior")',
+        in_modal: false,
+        extra_trigger: 'ul.ui-autocomplete',
+        run: 'click'
+    }, {
+        content: _t('Set default_start_date to 05/01/2020'),
+        trigger: '.o_form_editable .o_datepicker_input[name="default_start_date"]',
+        run: 'text 03/01/2020',
+        position: 'bottom',
+    }, {
+        content: _t('Set default_end_date to 07/01/2020'),
+        trigger: '.o_form_editable .o_datepicker_input[name="default_end_date"]',
+        run: 'text 05/01/2020',
+        position: 'bottom',
+    },{
+        content: _t("Click here to create or add Product"),
+        trigger: "a:contains('Add a product')",
+        position: "bottom",
+    }, {
+        trigger: ".o_field_many2one[name='display_product_id'] .o_input_dropdown input",
+        run: 'click'
+    }, {
+        trigger: 'li a:contains("Volvo L110H")',
+        in_modal: false,
+        extra_trigger: 'ul.ui-autocomplete',
+        run: 'click'
+    }, {
+        content: _t('Click on Save & Close'),
+        trigger: 'button span:contains(Save & Close)',
+        extra_trigger: '.o_act_window',
+        id: "rental_product_selected",
+        run: 'click',
+        position: 'bottom',
+    }, {
+        content: _t('Check total rental time'),
+        trigger: 'td.o_data_cell:contains("62.00")',
+        extra_trigger: 'div[name="order_line"]',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check (must be diff between 05/01/2020 and 07/01/2020)
+    }, {
+        content: _t('Check Unit of measure'),
+        trigger: 'td.o_data_cell:contains("Day(s)")',
+        extra_trigger: 'div[name="order_line"]',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check (must be Day(s))
+    }, {
+        content: _t('Check Unit Price'),
+        trigger: 'td.o_data_cell:contains("200.00")',
+        extra_trigger: 'div[name="order_line"]',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check
+    }, {
+        content: _t('Save Rental Order'),
+        trigger: '.o_form_button_save',
+        position: 'bottom',
+    }, {
+        content: _t("Click Send by email"),
+        trigger: 'button[name="action_quotation_send"]',
+    }, {
+        content: _t("Click Confirm"),
+        trigger: 'button[name="action_confirm"]',
+    }, {
+        content: _t("Click on Delivery"),
+        trigger: 'button[name="action_view_delivery"]',
+    }, {
+        content: _t('Check Outgoing Delivery'),
+        trigger: 'td.o_data_cell:contains("Rental/Rental Out")',
+        extra_trigger: '.o_list_view',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check there must be OUT deleivery
+    }, {
+        content: _t('Check Incoming Delivery'),
+        trigger: 'td.o_data_cell:contains("Rental/Rental In")',
+        extra_trigger: '.o_list_view',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check there must be IN deleivery
+    }, {
+        content: _t('Click on Outgoing Delivery'),
+        trigger: 'td.o_data_cell:contains("Rental/Rental Out")',
+        extra_trigger: '.o_list_view',
+        position: 'bottom',
+        run: 'click',
+    }, {
+        content: _t("Click on Validate"),
+        trigger: 'button[name="button_validate"]',
+    }, {
+        content: _t('Click on Apply'),
+        trigger: 'button span:contains("Apply")',
+        extra_trigger: '.o_act_window',
+        id: "rental_product_out",
+        run: 'click',
+        position: 'bottom',
+    }, {
+        content: _t('Check Done Quantity'),
+        trigger: 'td.o_data_cell:contains("1.000")',
+        extra_trigger: 'div[name="move_ids_without_package"]',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check Done: 1.0 Unit(s)
+    }, {
+        content: _t("Use the breadcrumbs to <b>go back to your Transfer</b>."),
+        trigger: ".breadcrumb-item:not(.active):nth-last-child(3)",
+        extra_trigger: '.o_form_view',
+        position: "bottom"
+    }, {
+        content: _t("Click on Create Invoice"),
+        trigger: '.o_statusbar_buttons button span:contains("Create Invoice")',
+    }, {
+        content: _t('Click on Create and View Invoice'),
+        trigger: 'button[name="create_invoices"]',
+        extra_trigger: '.o_act_window',
+        id: "rental_product_invoice",
+        run: 'click',
+        position: 'bottom',
+    }, {
+        content: _t('Check Sale Type on Invoice'),
+        trigger: '.o_invoice_form a[name="sale_type_id"]:contains("Rental Order")',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check Invoice: Sale Type = Rental Order
+    }, {
+        content: _t('Check Service Period from'),
+        trigger: '.o_invoice_form span[name="date_start"]:contains("03/01/2020")',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check Invoice: Service Period from 03/01/2020
+    }, {
+        content: _t('Check Service Period to'),
+        trigger: '.o_invoice_form span[name="date_end"]:contains("05/01/2020")',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check Invoice: Service Period to 05/01/2020
+    }, {
+        content: _t('Check total rental time on Invoice'),
+        trigger: 'td.o_data_cell:contains("62.00")',
+        extra_trigger: 'div[name="invoice_line_ids"]',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check Invoice: (Rental Time : 62 days)
+    }, {
+        content: _t('Check Unit of measure on Invoice'),
+        trigger: 'td.o_data_cell:contains("Day(s)")',
+        extra_trigger: 'div[name="invoice_line_ids"]',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check Invoice: (Uom : Day(s))
+    }, {
+        content: _t('Check Unit Price on Invoice'),
+        trigger: 'td.o_data_cell:contains("200.00")',
+        extra_trigger: 'div[name="invoice_line_ids"]',
+        in_modal: false,
+        position: 'bottom',
+        run: function (){} //check Invoice: (Unit Price : 200.00)
+    }, {
+        content: _t('Click on Validate'),
+        trigger: 'button[name="action_invoice_open"]',
+        position: 'bottom',
+    },
+  ]);
+});
--- test_rental_workflows/tests/test_rental_so.py
+++ test_rental_workflows/tests/test_rental_so.py
@@ -1,97 +1,96 @@
 # Part of rental-vertical See LICENSE file for full copyright and licensing details.
-
-from odoo.tests.common import TransactionCase
-from odoo.addons.rental_pricelist.tests.test_rental_pricelist import TestRentalPricelist
-from datetime import date, timedelta
-from odoo.tests import tagged
-
-import logging
-_logger = logging.getLogger(__name__)
+import odoo.tests
 
 
-@tagged('post_install', '-at_install')
-class TestRentalSO(TestRentalPricelist):
-    """ Test renting of a product instance for 60 days including quotation and order"""
-    def setUp(self):
-        super(TestRentalSO, self).setUp()
-        self.today = date.today()
-        self.date_60_day_later = self.today + timedelta(days=60)
-        self.rental_sale_type = self.env.ref('rental_base.rental_sale_type')
-        self.product = self.env.ref('rental_product_instance.rental_01530')
-        self.product.rental_service_ids.write({
-            'income_analytic_account_id': self.product.income_analytic_account_id,
-            'expense_analytic_account_id': self.product.expense_analytic_account_id})
-        self.partner = self.env.ref('base.res_partner_12')
-        # add scale price of product
-        #    Day     Price
-        #      1     200
-        #     50      70
-        self.item = self.env['product.pricelist.item'].create({
-            'applied_on': '0_product_variant',
-            'compute_price': 'fixed',
-            'product_id': self.product.product_rental_day_id.id,
-            'pricelist_id': self.product.def_pricelist_id.id,
-            'min_quantity': 50,
-            'fixed_price': 70,
-        })
-        # sale order with type : Rental Order
-        self.rental_order = self.env['sale.order'].with_context({
-            'default_type_id': self.rental_sale_type.id,
-        }).create({
-            'partner_id': self.partner.id,
-            'pricelist_id': self.env.ref('product.list0').id,
-        })
-        line = self.env['sale.order.line'].with_context({
-            'type_id': self.rental_sale_type.id,
-        }).new({
-            'order_id': self.rental_order.id,
-            'display_product_id': self.product.id,
-            'start_date': self.today,
-            'end_date': self.date_60_day_later,
-        })
-        line.onchange_display_product_id()
-        res = line.product_id_change()
-        # Feature : check available uom coming from product
-        check_uom_domain = False
-        if 'domain' in res and 'product_uom' in res['domain']:
-            self.assertEqual(len(res['domain']['product_uom'][0][2]), 2)
-            check_uom_domain = True
-        self.assertTrue(check_uom_domain)
-        line.onchange_rental()
-        line.product_uom_change()
-        line.rental_product_id_change()
-        self._run_sol_onchange_date(line)
-        vals = line._convert_to_write(line._cache)
-        vals['order_id'] = self.rental_order.id
-        self.line = self.env['sale.order.line'].create(vals)
+@odoo.tests.tagged('post_install', '-at_install')
+class TestUi(odoo.tests.HttpCase):
 
-    def test_02_check_price_scale(self):
-        self.item._onchange_product_id()
-        # Feature : check price scales on Product
-        self.assertEqual(self.item.day_item_id, self.product)
-        # Feature : check price scales while adding sol
-        self.assertEqual(self.line.product_uom_qty > 50, True)
-        self.assertEqual(self.line.price_unit, 70)
-        self._run_sol_onchange_date(self.line, end_date=self.date_60_day_later)
-        self._run_sol_onchange_date(self.line, end_date=self.tomorrow)
-        self.assertEqual(self.line.product_uom_qty, 2)
-        self.assertEqual(self.line.price_unit, 200)
+    def test_01_rental_tour(self):
+        self.phantom_js("/web", "odoo.__DEBUG__.services['web_tour.tour'].run('rental_tour', 1000)", "odoo.__DEBUG__.services['web_tour.tour'].tours.rental_tour.ready", login="admin")
 
-    def test_rental_so_to_invoice(self):
-        self.rental_order.action_confirm()
-        # create an invoice with invoiceable lines
-        wiz_inv = self.env['sale.advance.payment.inv'].with_context(
-            active_ids=[self.rental_order.id]).create({
-            'advance_payment_method': 'delivered',
-        })
-        wiz_inv.create_invoices()
-        self.invoice = self.rental_order.invoice_ids[0]
-        self.invoice.action_invoice_open()
-        # Feature : check Sale type on invoice
-        self.assertEqual(self.invoice.sale_type_id, self.rental_sale_type,
-            "Sale Type of Invoice must be same as on Sale Order")
-        # Feature : check analytic account on invoice line
-        for inv_line in self.invoice.invoice_line_ids:
-            self.assertEquals(
-                inv_line.account_analytic_id,
-                inv_line.product_id.income_analytic_account_id)
+#@tagged('post_install', '-at_install')
+#class TestRentalSO(TestRentalPricelist):
+#    """ Test renting of a product instance for 60 days including quotation and order"""
+#    def setUp(self):
+#        super(TestRentalSO, self).setUp()
+#        self.today = date.today()
+#        self.date_60_day_later = self.today + timedelta(days=60)
+#        self.rental_sale_type = self.env.ref('rental_base.rental_sale_type')
+#        self.product = self.env.ref('rental_product_instance.rental_01530')
+#        self.product.rental_service_ids.write({
+#            'income_analytic_account_id': self.product.income_analytic_account_id,
+#            'expense_analytic_account_id': self.product.expense_analytic_account_id})
+#        self.partner = self.env.ref('base.res_partner_12')
+#        # add scale price of product
+#        #    Day     Price
+#        #      1     200
+#        #     50      70
+#        self.item = self.env['product.pricelist.item'].create({
+#            'applied_on': '0_product_variant',
+#            'compute_price': 'fixed',
+#            'product_id': self.product.product_rental_day_id.id,
+#            'pricelist_id': self.product.def_pricelist_id.id,
+#            'min_quantity': 50,
+#            'fixed_price': 70,
+#        })
+#        # sale order with type : Rental Order
+#        self.rental_order = self.env['sale.order'].with_context({
+#            'default_type_id': self.rental_sale_type.id,
+#        }).create({
+#            'partner_id': self.partner.id,
+#            'pricelist_id': self.env.ref('product.list0').id,
+#        })
+#        line = self.env['sale.order.line'].with_context({
+#            'type_id': self.rental_sale_type.id,
+#        }).new({
+#            'order_id': self.rental_order.id,
+#            'display_product_id': self.product.id,
+#            'start_date': self.today,
+#            'end_date': self.date_60_day_later,
+#        })
+#        line.onchange_display_product_id()
+#        res = line.product_id_change()
+#        # Feature : check available uom coming from product
+#        check_uom_domain = False
+#        if 'domain' in res and 'product_uom' in res['domain']:
+#            self.assertEqual(len(res['domain']['product_uom'][0][2]), 2)
+#            check_uom_domain = True
+#        self.assertTrue(check_uom_domain)
+#        line.onchange_rental()
+#        line.product_uom_change()
+#        line.rental_product_id_change()
+#        self._run_sol_onchange_date(line)
+#        vals = line._convert_to_write(line._cache)
+#        vals['order_id'] = self.rental_order.id
+#        self.line = self.env['sale.order.line'].create(vals)
+#
+#    def test_02_check_price_scale(self):
+#        self.item._onchange_product_id()
+#        # Feature : check price scales on Product
+#        self.assertEqual(self.item.day_item_id, self.product)
+#        # Feature : check price scales while adding sol
+#        self.assertEqual(self.line.product_uom_qty > 50, True)
+#        self.assertEqual(self.line.price_unit, 70)
+#        self._run_sol_onchange_date(self.line, end_date=self.date_60_day_later)
+#        self._run_sol_onchange_date(self.line, end_date=self.tomorrow)
+#        self.assertEqual(self.line.product_uom_qty, 2)
+#        self.assertEqual(self.line.price_unit, 200)
+#
+#    def test_rental_so_to_invoice(self):
+#        self.rental_order.action_confirm()
+#        # create an invoice with invoiceable lines
+#        wiz_inv = self.env['sale.advance.payment.inv'].with_context(
+#            active_ids=[self.rental_order.id]).create({
+#            'advance_payment_method': 'delivered',
+#        })
+#        wiz_inv.create_invoices()
+#        self.invoice = self.rental_order.invoice_ids[0]
+#        self.invoice.action_invoice_open()
+#        # Feature : check Sale type on invoice
+#        self.assertEqual(self.invoice.sale_type_id, self.rental_sale_type,
+#            "Sale Type of Invoice must be same as on Sale Order")
+#        # Feature : check analytic account on invoice line
+#        for inv_line in self.invoice.invoice_line_ids:
+#            self.assertEquals(
+#                inv_line.account_analytic_id,
+#                inv_line.product_id.income_analytic_account_id)
