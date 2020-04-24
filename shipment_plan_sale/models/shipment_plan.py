b7dcb59a386c1b6ace4165768a3c62f4fd486a37
Yu Weng <yweng@elegosoft.com>
2020-04-24 21:00:25 +0200

    [IMP] Relation between shipment.plan and stock.picking

--- shipment_plan_sale/models/shipment_plan.py
+++ shipment_plan_sale/models/shipment_plan.py
@@ -22,8 +22,30 @@ class ShipmentPlan(models.Model):
         'Origin Sale Order',
         compute="_compute_sale_id"
     )
+    picking_ids = fields.Many2many(
+        'stock.picking',
+        compute='_compute_picking_ids'
+    )
+    picking_count = fields.Integer(
+        compute="_compute_picking_ids"
+    )
 
     def _compute_sale_id(self):
         for record in self:
             if record.origin_sale_line_ids:
                 record.sale_id = record.origin_sale_line_ids[0].order_id
+
+    def _compute_picking_ids(self):
+        for record in self:
+            pickings = self.env['stock.picking'].browse()
+            for line in record.origin_sale_line_ids:
+                for move in line.move_ids:
+                    if move.picking_id:
+                        pickings |= move.picking_id
+            record.picking_ids = pickings
+            record.picking_count = len(pickings)
+
+    def action_view_pickings(self):
+        action = self.env.ref('stock.action_picking_tree_all').read()[0]
+        action['domain'] = [('id', 'in', self.picking_ids.ids)]
+        return action
