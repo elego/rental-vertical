70c4b7df07fb55db41a3009d3d3c6f8342ca59b8
Yurdik Cervantes <ycervantes@elegosoft.com>
2020-04-28 20:52:29 +0200

    (issue #3478) fix labels and translations for the toll.charge.line fields
    - once the odoo import works correctly with the sample file,
      addditional parameters might be added

--- rental_toll_collect/models/toll_charge_line.py
+++ rental_toll_collect/models/toll_charge_line.py
@@ -1,5 +1,5 @@
 # Part of rental-vertical See LICENSE file for full copyright and licensing details.
-
+from datetime import datetime as dt, timedelta
 
 from odoo import api, fields, models, exceptions, _
 
@@ -13,19 +13,21 @@ class TollChargeLine(models.Model):
 
 
     vehicle_reg_number = fields.Char(string='Vehicle Registration Number')
-    start_date = fields.Datetime(string='Start Date')
+    start_date = fields.Datetime(string='Date')
+    start_time = fields.Char(string='Start')
+    start_dt = fields.Datetime(string='Start Datetime', compute='_compute_start_dt')
     booking_number = fields.Char(string='Booking Number')
     toll_type = fields.Selection([('toll', 'Toll')], string='Type')
     drive = fields.Char(string='Drive')
     drive_via = fields.Char(string='Drive via')
-    departure = fields.Char(string='Deaprture')
+    departure = fields.Char(string='Departure')
     cost_centre = fields.Many2one('account.analytic.account',
-        string='Cost Centre',
+        string='Cost Center',
         default=lambda self: self._default_analytic_account())
     tariff_model = fields.Char(string='Tariff Model')
     axis_class = fields.Char(string='Axis Class')
     weight_class = fields.Char(string='Weight Class')
-    polution_class = fields.Char(string='Poution Class')
+    polution_class = fields.Char(string='Pollution Class')
     road_operator = fields.Many2one('res.partner', string='Road Operator')
     procedure = fields.Selection(
         [('AV', 'Automatic Procedure'),
@@ -33,7 +35,7 @@ class TollChargeLine(models.Model):
          ('MVI', 'Manual Procedure Internet'),
          ('MVA', 'Manual Procedure App')],
         string='Procedure')
-    kilometer = fields.Float(string='KiloMeter')
+    kilometer = fields.Float(string='Kilometer')
     toll_charge = fields.Float(string='Toll Charge')
     is_charges = fields.Boolean(string='Is Charged', default=False)
     toll_product_id = fields.Many2one('product.product',
@@ -42,3 +44,17 @@ class TollChargeLine(models.Model):
         string='Toll Contract')
     toll_invoice_id = fields.Many2one('account.invoice',
         string='Toll Invoice')
+
+    @api.multi
+    @api.depends('start_date', 'start_time')
+    def _compute_start_dt(self):
+        for charge_line in self:
+            if meeting.start_date:
+                if meeting.start_time:
+                    start = dt.strptime(meeting.start_time, "%H:%M")
+                    offset = start - dt.strptime("00:00", "%H:%M")
+                    meeting.start_dt = meeting.start_date + offset
+                else:
+                    meeting.start_dt = meeting.start_date
+            else:
+                meeting.start_dt = False
