c5dbd032537c1567492e45d92a47c4c2b406387a
Olaf Wagner <wagner@elegosoft.com>
2020-05-16 14:47:55 +0200

    remove crm dependency from rental_base (issue #3339)

--- rental_base/__manifest__.py
+++ rental_base/__manifest__.py
@@ -28,11 +28,10 @@ Please activate the checkbox for using 'Product Variants' in Sales > Configurati
 Otherwise you can not deal with rental orders.
 ''',
     'version': '12.0.1.0.1',
-    'category': 'product',
+    'category': 'Rental',
     'author': 'Odoo Community Association (OCA)/Elego Software Solutions GmbH',
     'depends': [
         'account',
-        'crm',
         'product_analytic',
         'product_dimension',
         'prt_product_tweaks',
