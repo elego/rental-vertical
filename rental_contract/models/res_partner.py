74abd2c7fa4dd14cb7c76bdc0cef14712516c92d
Yu Weng <yweng@elegosoft.com>
2020-09-02 13:27:59 +0200

    [IMP] adjust Menus for contracts

--- rental_contract/models/res_partner.py
+++ rental_contract/models/res_partner.py
@@ -9,3 +9,12 @@ class ResPartner(models.Model):
     contract_type = fields.Many2one(
         comodel_name='contract.order.type', string='Contract Subtype',
         company_dependent=True)
+
+    def act_show_contract(self):
+        """ This opens contract view
+            @return: the contract view
+        """
+        self.ensure_one()
+        res = super().act_show_contract()
+        res['context'].update(search_default_group_by_type_id=1)
+        return res
