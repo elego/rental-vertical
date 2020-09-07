abf3a986bc1a53fa23b1b06a6c78c71041fa7cfa
Yu Weng <yweng@elegosoft.com>
2020-09-07 14:16:44 +0200

    [IMP] implements new model instance.operating.data to save the instance condition kg / hour (Issue 3641)

--- rental_product_instance/__manifest__.py
+++ rental_product_instance/__manifest__.py
@@ -7,7 +7,7 @@
 '''Model product is extended as an instance that can be traced by serial number.
 An instance can only have one serial number, so that it is unique in the system.
 It can be used in rental orders, repair orders and maintenance orders.''',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Rental',
     'author': 'Odoo Community Association (OCA)/Elego Software Solutions GmbH',
     'depends': [
@@ -15,7 +15,9 @@ It can be used in rental orders, repair orders and maintenance orders.''',
         'rental_timeline',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/product_view.xml',
+        'views/instance_operating_data_view.xml',
     ],
     'demo': [
     ],
