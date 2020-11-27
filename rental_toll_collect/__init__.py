4be58b634740b01aacfda75660f20f0394a21118
Maria Sparenberg <maria.sparenberg@elegosoft.com>
2020-11-27 13:45:12 +0100

    issue #3950 set the charge product for all partners

--- rental_toll_collect/__init__.py
+++ rental_toll_collect/__init__.py
@@ -8,9 +8,7 @@ from odoo.api import Environment, SUPERUSER_ID
 
 def post_init_hook(cr, registry):
     env = Environment(cr, SUPERUSER_ID, {})
-    customers = env['res.partner'].search([
-        ('customer', '=', True),
-    ])
+    customers = env['res.partner'].search([])
     customers.write({
         'administrative_charge_product': env.ref('rental_toll_collect.product_administrative_charge').id,
     })
