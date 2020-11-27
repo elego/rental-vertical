01f153014e84484fd73a14b0bd143c5f68b68eff
Maria Sparenberg <maria.sparenberg@elegosoft.com>
2020-11-27 09:26:29 +0100

    issue #3950 fix toll collect webtour by saving settings with execute button

--- test_rental_workflows/static/src/js/test_rental_contract_toll_collect_tour.js
+++ test_rental_workflows/static/src/js/test_rental_contract_toll_collect_tour.js
@@ -32,12 +32,8 @@ odoo.define('contract_toll_collect.tour', function (require) {
             run: function (){
                 $('div.o_field_boolean[name="automatic_toll_charge_invoicing"] input[type="checkbox"]').prop("checked", true);
                 $('div.o_field_boolean[name="automatic_toll_charge_invoicing"] input[type="checkbox"]').trigger('change');
-                }
-        },
-        {
-            content: _t('Save settings'),
-            trigger: '.btn-primary',
-            position: 'bottom',
+                $('.o_statusbar_buttons button[name="execute"]').click();
+                },
         },
         {
             content: _t('Go to top menu of Customer'),
