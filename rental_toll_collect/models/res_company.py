363aa9be9959fcbaaf086c5b7ef9e4508578015a
cpatel <cpatel@elegosoft.com>
2020-12-08 17:47:46 +0100

    [IMP] manage Administrative Charge product for rental_toll_collect, (issue#3950)

--- rental_toll_collect/models/res_company.py
+++ rental_toll_collect/models/res_company.py
@@ -11,3 +11,34 @@ class ResCompany(models.Model):
              "automatically invoiced when invoicing a sale "
              "or rental order or contract.",
     )
+    administrative_charge = fields.Boolean(
+        string="Administrative Charge",
+        help="If activated,you can set your own default "
+        "administrative charge product for the partners.",
+        default=True,
+    )
+    administrative_charge_product = fields.Many2one(
+        comodel_name="product.product",
+        string="Administrative Charge Product",
+        default=lambda self: self.env.ref(
+            'rental_toll_collect.product_administrative_charge',
+            raise_if_not_found=False
+        ),
+        domain=[('type', '=', 'service')],
+        help="Product that can be used as default "
+        "administrative charge product for the partners.",
+    )
+
+    @api.model
+    def create(self, values):
+        company = super(ResCompany, self).create(values)
+        company._set_administrative_charge_product()
+        return company
+
+    def _set_administrative_charge_product(self):
+        for company in self:
+            if not company.administrative_charge_product:
+                product = self.env.ref('rental_toll_collect.product_administrative_charge')
+                company.write({
+                    'administrative_charge_product': product.id,
+                })
