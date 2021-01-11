eb2e57bda0df8a12b4cd31dd23277cc05473c874
Maria Sparenberg <maria.sparenberg@elegosoft.com>
2021-01-11 14:03:33 +0100

    issue #3978 add several additional off days in sale order line form view (not tree view)

--- rental_offday/__manifest__.py
+++ rental_offday/__manifest__.py
@@ -4,12 +4,12 @@
     "name": "Rental Off Day",
     "summary": "Calculate off-days in rentals on daily basis",
     "description": """
-During short-term rentals over several days or weeks, the customer and the salesman 
-agree on so called off-days. On these days the customer still have the rented products 
-but usually doesn't use them and, therefore, does not pay the daily price. This is often 
-the case for weekends and holidays, since there might be some legal limitations in using 
+During short-term rentals over several days or weeks, the customer and the salesman
+agree on so called off-days. On these days the customer still have the rented products
+but usually doesn't use them and, therefore, does not pay the daily price. This is often
+the case for weekends and holidays, since there might be some legal limitations in using
 the products on these days.
-In order to meet this requirement, the salesman can add off-days on sale order lines for 
+In order to meet this requirement, the salesman can add off-days on sale order lines for
 products that are rentable in days. These days will not be included in price calculation.
 """,
     "usage": """
@@ -20,8 +20,8 @@ The off-days can only be used for products rentable in days.
 - Create a sale order and rent out the product in days.
 - Set a start and end date, e.g. for 3 weeks.
 - On sale order line you will see a page 'Off-Days'.
-- Choose the type 'Weekend' in order to create 'Fixed Off-Days' and you get a list with all saturdays 
-  and sundays within the rental period.   
+- Choose the type 'Weekend' in order to create 'Fixed Off-Days' and you get a list with all saturdays
+  and sundays within the rental period.
 - Add some additional off-days.
 - The number of off-days reduces the rental quantity and is therefore not included in price calculation.
 
@@ -34,7 +34,6 @@ The off-days can only be used for products rentable in days.
     ],
     "data": [
         "security/ir.model.access.csv",
-        "wizard/add_offday_view.xml",
         "views/sale_view.xml",
     ],
     "demo": [],
