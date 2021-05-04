72fea78298f2b3d6c931a89e23b59305ed41669d
Yu Weng <yweng@elegosoft.com>
2021-05-04 10:59:46 +0200

    [FIX] Unittest of rental_pricelist_interval

--- rental_pricelist_interval/models/sale.py
+++ rental_pricelist_interval/models/sale.py
@@ -124,13 +124,19 @@ class SaleOrderLine(models.Model):
         if (
             self.order_id.pricelist_id.is_interval_pricelist
             and self.product_id
-            and self.product_id.rented_product_id
         ):
-            if not self.product_id.rented_product_id.rental_of_interval:
+            if (
+                not self.product_id.rented_product_id
+                or not self.product_id.rented_product_id.rental_of_interval
+            ):
                 raise exceptions.UserError(
                     _("No interval price was defined for this product.")
                 )
-            if self.rental and "domain" in res and "product_uom" in res["domain"]:
+            if (
+                self.product_id.rented_product_id
+                and self.rental and "domain" in res
+                and "product_uom" in res["domain"]
+            ):
                 del res["domain"]["product_uom"]
                 if self.display_product_id.rental:
                     uom_ids = [uom_interval.id]
