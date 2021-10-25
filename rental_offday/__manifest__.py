ce77a3231e55badf66963225201802e10de85855
Olaf Wagner <wagner@elegosoft.com>
2021-10-25 10:17:16 +0200

    fix two syntax errors in documentation (issue #3339)

--- rental_offday/__manifest__.py
+++ rental_offday/__manifest__.py
@@ -37,8 +37,7 @@ Create a rental order:
  * Set the quantity to rent out one or several storable rentable products.
  * Choose start and end date, e.g. for 3 weeks.
  * On the order line you will see a page 'Off-Days' at the bottom.
- * Choose the type 'Weekend' in order to create 'Fixed Off-Days' and you get a list with all saturdays
- and sundays within the rental period.
+ * Choose the type 'Weekend' in order to create 'Fixed Off-Days' and you get a list with all saturdays and sundays within the rental period.
  * Add some additional off-days as needed.
  * The number of off-days reduces the rental quantity and is therefore not included in price calculation.
  * Confirm the order.
