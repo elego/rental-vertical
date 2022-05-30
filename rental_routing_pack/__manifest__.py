bef166a68b3dec65bd1d10ab69a1a4a02ee2bad9
Yu Weng <yweng@elegosoft.com>
2022-05-30 14:25:42 +0200

    [ADD] Module rental_routing_pack (issue #4721)

---
+++ rental_routing_pack/__manifest__.py
@@ -0,0 +1,32 @@
+# Part of rental-vertical See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Rental Routing Pack",
+    "version": "12.0.1.0.0",
+    "category": "Rental",
+    "summary": "Rental Routing Pack",
+    "description": """
+This module allows to routing rentals with product packs.
+You can plan you rentals to bypass the product in pack (without pack) between customers.
+""",
+    "usage": """
+Install the module.
+No further configuration is needed.
+""",
+
+    "author": "elego Software Solutions GmbH, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/vertical-rental",
+    "depends": [
+        "rental_product_pack",
+        "rental_routing",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_view.xml",
+        "wizard/rental_pack_product_route_view.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+    "application": False,
+    "license": "AGPL-3",
+}
