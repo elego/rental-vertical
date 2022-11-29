c922b69dfb1f84c552ddde6922f05e0aba6a02fc
chandni299 <chandnip445@gmail.com>
2022-11-29 15:11:47 +0100

    [IMP] todo points pass correct information of PO line to SO line for rental type order,(issue#5221)

--- purchase_sale_rental_intercompany/models/purchase_order.py
+++ purchase_sale_rental_intercompany/models/purchase_order.py
@@ -3,6 +3,7 @@ from odoo.exceptions import UserError
 import logging
 
 _logger = logging.getLogger(__name__)
+from odoo.tools import float_round
 
 
 class PurchaseOrder(models.Model):
@@ -28,8 +29,17 @@ class PurchaseOrder(models.Model):
             self, purchase_line, dest_company, sale_order):
         new_line = super(PurchaseOrder, self)._prepare_sale_order_line_data(purchase_line, dest_company, sale_order)
         new_line.update({
-            'start_date': purchase_line.start_date,
-            'end_date': purchase_line.end_date,
+            "start_date": purchase_line.start_date,
+            "end_date": purchase_line.end_date,
+            "rental": True,
+            "product_uom": purchase_line.product_uom.id,
+            "price_unit": purchase_line.price_unit,
+            "product_uom_qty": purchase_line._get_number_of_time_unit(),
+            "number_of_time_unit": purchase_line._get_number_of_time_unit(),
+            "rental_qty": purchase_line.product_qty,
+            "order_type": "rental",
+            "rental_type": "new_rental",
+            "display_product_id": purchase_line.product_id.rented_product_id.id,
         })
         return new_line
 
@@ -38,3 +48,59 @@ class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     state = fields.Selection(related='order_id.state', store=True, readonly=False, default='draft')
+
+    @api.model
+    def _get_time_uom(self):
+        uom_month = self.env.ref("rental_base.product_uom_month")
+        uom_day = self.env.ref("uom.product_uom_day")
+        uom_hour = self.env.ref("uom.product_uom_hour")
+        return {
+            "month": uom_month,
+            "day": uom_day,
+            "hour": uom_hour,
+        }
+
+    @api.multi
+    def _get_number_of_time_unit(self):
+        self.ensure_one()
+        number = False
+        time_uoms = self._get_time_uom()
+        if self.product_uom.id == time_uoms["day"].id:
+            number = (self.end_date - self.start_date).days + 1
+        elif self.product_uom.id == time_uoms["hour"].id:
+            number = ((self.end_date - self.start_date).days + 1) * 8
+        elif self.product_uom.id == time_uoms["month"].id:
+            # ref link to calculate months (why 30.4167 ?)
+            # https://www.checkyourmath.com/convert/time/days_months.php
+            number = ((self.end_date - self.start_date).days + 1) / 30.4167
+            number = float_round(number, precision_rounding=1)
+        return number
+
+    @api.multi
+    def _get_product_rental_uom_ids(self):
+        self.ensure_one()
+        time_uoms = self._get_time_uom()
+        uom_ids = []
+        if self.product_id.rented_product_id.rental_of_month:
+            uom_ids.append(time_uoms["month"].id)
+        if self.product_id.rented_product_id.rental_of_day:
+            uom_ids.append(time_uoms["day"].id)
+        if self.product_id.rented_product_id.rental_of_hour:
+            uom_ids.append(time_uoms["hour"].id)
+        return uom_ids
+
+    @api.onchange("product_id")
+    def onchange_product_id(self):
+        res = super().onchange_product_id()
+        if (
+            self.product_id.rental
+            and self.product_id.rented_product_id
+            and self.product_id.rented_product_id.rental
+        ):
+            if "domain" in res and "product_uom" in res["domain"]:
+                del res["domain"]["product_uom"]
+                uom_ids = self._get_product_rental_uom_ids()
+                res["domain"]["product_uom"] = [("id", "in", uom_ids)]
+                if uom_ids and self.product_uom.id not in uom_ids:
+                    self.product_uom = uom_ids[0]
+        return res
