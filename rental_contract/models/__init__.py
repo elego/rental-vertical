d8481d06b4f23b0a2e5dba5618ff6dd389fe9c59
Yu Weng <yweng@elegosoft.com>
2025-07-16 09:32:17 +0200

    [DEL] module rental_contract, rental_contract_insurance (issue #6981)

--- rental_contract/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Part of rental-vertical See LICENSE file for full copyright and licensing details.
-from . import contract_order_type
-from . import contract
-from . import contract_line
-from . import sale
-from . import product
-from . import res_partner
-from . import account_invoice
